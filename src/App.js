dcac5e569d0961591d902e717079033239ff3bc4
kevin-ho87 <31581926+kevin-ho87@users.noreply.github.com>
2017-12-10 16:27:49 +1100

    Structure start
    2 column structure, Google font embed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <div className="col">
+          <h3>Results</h3>
+          <div className="cards-holder">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat accusamus blanditiis nobis labore quis magnam aut ut, possimus natus sed minima, dolore amet, iste pariatur cumque eligendi beatae consequuntur qui.</p>
+          </div>
+        </div>
+        <div className="col">
+          <h3>Saved Properties</h3>
+          <div className="cards-holder">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat accusamus blanditiis nobis labore quis magnam aut ut, possimus natus sed minima, dolore amet, iste pariatur cumque eligendi beatae consequuntur qui.</p>
+          </div>
+        </div>
       </div>
     );
   }

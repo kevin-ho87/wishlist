c88283c85f6a261e7e1ecb9427374961197bf47d
kevin-ho87 <31581926+kevin-ho87@users.noreply.github.com>
2017-12-10 18:12:33 +1100

    Results cards
    Displaying cards by looping through 'results' data

--- src/App.js
+++ src/App.js
@@ -14,25 +14,31 @@ class App extends Component {
   }
 
   render() {
+    const resultsCards = this.state.results.map((item) => {
+      return (
+        <div className="card" key={item.id}>
+          <header className="card__header" style={{backgroundColor: item.agency.brandingColors.primary}}>
+            <img src={item.agency.logo} alt=""/>
+          </header>
+          <div className="card__thumb">
+            <img src={item.mainImage} alt=""/>
+          </div>
+          <footer className="card__footer">
+            <p>Price: {item.price}</p>
+          </footer>
+          <div className="card__overlay">
+            <button className="btn btn_add" type="button">Add property</button>
+          </div>
+        </div>
+      );
+    });
+
     return (
       <div className="container">
         <div className="col">
           <h3>Results</h3>
           <div className="cards-holder">
-            <div className="card">
-              <header className="card__header" style={{backgroundColor: '#ffe512'}}>
-                <img src="http://i1.au.reastatic.net/agencylogo/XRWXMT/12/20120927204448.gif" alt=""/>
-              </header>
-              <div className="card__thumb">
-                <img src="http://i2.au.reastatic.net/640x480/20bfc8668a30e8cabf045a1cd54814a9042fc715a8be683ba196898333d68cec/main.jpg" alt=""/>
-              </div>
-              <footer className="card__footer">
-                <p>Price: $726,500</p>
-              </footer>
-              <div className="card__overlay">
-                <button className="btn btn_add" type="button">Add property</button>
-              </div>
-            </div>
+            { resultsCards }
           </div>
         </div>
         <div className="col">

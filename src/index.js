9a182e5b28467dbaee559da35131e1cf5828168a
kevin-ho87 <31581926+kevin-ho87@users.noreply.github.com>
2017-12-10 17:44:14 +1100

    Data
    Adding mockup JSON data and passing it to App component as props

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
+import data from './data/Data';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App data={data} />, document.getElementById('root'));
 registerServiceWorker();

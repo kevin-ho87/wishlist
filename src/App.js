4fbd965eddc2f8c9670ec91d79d35e0a1f6747e5
kevin-ho87 <31581926+kevin-ho87@users.noreply.github.com>
2017-12-10 18:19:27 +1100

    Saved cards
    Displaying cards by looping through 'saved' state data

--- src/App.js
+++ src/App.js
@@ -33,6 +33,25 @@ class App extends Component {
       );
     });
 
+    const savedCards = this.state.saved.map((item) => {
+      return (
+        <div className="card" key={item.id}>
+          <header className="card__header" style={{backgroundColor: item.agency.brandingColors.primary}}>
+            <img src={item.agency.logo} alt=""/>
+          </header>
+          <div className="card__thumb">
+            <img src={item.mainImage} alt=""/>
+          </div>
+          <footer className="card__footer">
+            <p>Price: {item.price}</p>
+          </footer>
+          <div className="card__overlay">
+            <button className="btn btn_remove" type="button">Remove property</button>
+          </div>
+        </div>
+      );
+    });
+
     return (
       <div className="container">
         <div className="col">
@@ -44,20 +63,7 @@ class App extends Component {
         <div className="col">
           <h3>Saved Properties</h3>
           <div className="cards-holder">
-            <div className="card">
-              <header className="card__header" style={{backgroundColor: '#ffe512'}}>
-                <img src="http://i1.au.reastatic.net/agencylogo/XRWXMT/12/20120927204448.gif" alt=""/>
-              </header>
-              <div className="card__thumb">
-                <img src="http://i2.au.reastatic.net/640x480/20bfc8668a30e8cabf045a1cd54814a9042fc715a8be683ba196898333d68cec/main.jpg" alt=""/>
-              </div>
-              <footer className="card__footer">
-                <p>Price: $726,500</p>
-              </footer>
-              <div className="card__overlay">
-                <button className="btn btn_remove" type="button">Remove property</button>
-              </div>
-            </div>
+            { savedCards }
           </div>
         </div>
       </div>

0e3edb4e441165eb44d20c4744f1e7ec74e767d4
kevin-ho87 <31581926+kevin-ho87@users.noreply.github.com>
2017-12-11 19:46:42 +1100

    Button component
    Refactor 'Button' component slightly

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,17 +4,21 @@ class Button extends Component {
   render() {
     const { type } = this.props;
 
-    let button = null;
+    let buttonClass = '';
+    let buttonText = '';
+
     if (type === 'add') {
-      button = <button className="btn btn_add" type="button">Add property</button>;
+      buttonClass = 'btn_add';
+      buttonText = 'Add property';
     } else if (type === 'remove') {
-      button = <button className="btn btn_remove" type="button">Remove property</button>;
+      buttonClass = 'btn_remove';
+      buttonText = 'Remove property';
     }
 
     return (
-      <div>
-        {button}
-      </div>
+      <button className={`btn ${buttonClass}`} type="button">
+        {buttonText}
+      </button>
     );
   }
 }

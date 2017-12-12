4b084f23ff2395552f4a75b4f6418908bad830d4
kevin-ho87 <31581926+kevin-ho87@users.noreply.github.com>
2017-12-12 20:24:37 +1100

    Testing start
    Install and setup 'enzyme' for testing.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+import data from './data/Data';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  shallow(<App data={data} />);
 });
+
